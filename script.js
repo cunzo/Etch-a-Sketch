4e74e44ddbe7a065a56c3d562d7247f120994750
cunzo <58085535+cunzo@users.noreply.github.com>
2023-12-30 00:26:50 +0100

    Add some logic and style - 99% complete

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const titleContainer = document.querySelector(".titleContainer");
+const containerGrid = document.querySelector(".container");
+const title = document.querySelector("#title");
+const btnStart = document.querySelector(".btnStart");
+btnStart.addEventListener("click", setStartSize);
+
+function setStartSize(params) {
+  const size = prompt("Inserisci la grandezza della griglia");
+  if (size<=20 && size>=1) {
+    createGrid(size);
+    btnStart.style.display = "none";
+    const btnSetNewSize = document.createElement("button", "setNewSize");
+    btnSetNewSize.textContent = "Set new size!";
+    titleContainer.appendChild(btnSetNewSize);
+    btnSetNewSize.addEventListener("click", setNewSize);
+    titleContainer.style.display = "flex";
+    titleContainer.style.justifyContent = "center";
+    title.style.paddingRight = "10px";
+    btnStart.style.alignSelf = "center";
+    containerGrid.style.display = "inline";
+  } else {
+    alert("Inserisci un numero compreso tra 1 e 20");
+  }
+
+}
+
+function setNewSize(params) {
+  const newSize = prompt("Inserisci la grandezza della griglia");
+  if (newSize<=20 && newSize>=1) {
+    createGrid(newSize);
+  } else {
+    alert("Inserisci un numero compreso tra 1 e 20");
+  }
+}
+
 function createGrid(size) {
   const container = document.querySelector(".container");
   container.innerHTML = ''; // Pulisce il contenitore esistente prima di creare una nuova griglia
@@ -12,7 +47,25 @@ function createGrid(size) {
     }
     container.appendChild(rowDiv);
   }
+
+  const squareGrid = document.querySelectorAll(".square");
+
+  squareGrid.forEach(square => {
+    square.addEventListener("mouseover", () => {
+    square.style.backgroundColor = "black";
+    })
+  });
+
+  squareGrid.forEach(square => {
+    square.addEventListener("mouseout", () => {
+    square.style.backgroundColor = "white";
+    })
+  });
+
+
 }
 
-createGrid(16);
+
+
+
 
